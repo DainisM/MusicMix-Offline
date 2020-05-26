6246552e2c326a72d233a3d9658bd90ac75d0e4e
Dainis DM. Moisejenko <moisejenko11@gmail.com>
2020-05-26 14:25:38 +0200

    Started working on Playlist screen

--- screens/playlists.js
+++ screens/playlists.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, AsyncStorage } from 'react-native';
 
-export default function Playlists() {
-    return (
-        <View style={styles.container}>
-            <Text>Playlists screen</Text>
-        </View>
-    )
+export default class Playlists extends React.Component {
+
+    async componentDidMount() {
+        const allPlaylists = await AsyncStorage.getItem('playlists');
+
+            console.log(allPlaylists);
+    }
+
+    createPlaylist = async () => {
+        await AsyncStorage.setItem('playlists', 'PlaylistName')
+            .then(() => {
+                this.props.navigation.replace('Playlists');
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+    
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.playlistButton} onPress={this.createPlaylist}>
+                        <Text style={styles.playlistBtnText}>Create playlist</Text>
+                    </TouchableOpacity>
+                </View>
+    
+                <Text>Playlists screen</Text>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#ccfffd',
     },
+    buttonContainer: {
+        padding: 30,
+        alignItems: 'flex-end'
+    },
+    playlistButton: {
+        width: 140,
+        borderRadius: 30,
+        padding: 10,
+        backgroundColor: 'darkcyan'
+    },
+    playlistBtnText: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
   });
