cac0720465fb3b10288a9789b0383d60cb35f2f8
Dainis DM. Moisejenko <moisejenko11@gmail.com>
2020-05-26 11:46:20 +0200

    Made a audio player with controls

--- components/songModal.js
+++ components/songModal.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 import { Dimensions, View, Text, StyleSheet, Image, Modal, TouchableHighlight } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import Slider from 'react-native-slider';
-import { Audio } from 'expo-av';
+
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
+const BACKGROUND_COLOR = '#ccfffd';
+const DISABLED_OPACITY = 0.5;
+const FONT_SIZE = 18;
+const BUFFERING_STRING = '00:00';
 
 export default class Tracks extends Component {
     constructor(props) {
@@ -16,29 +21,142 @@ export default class Tracks extends Component {
 
         return (
             <View>
-
                 <Modal 
                     animationType= 'slide'
                     visible={modalVisible}
                 >
                     <View style={styles.container}>
+
+                        {/* Back icon to close modal onPress */}
                         <MaterialIcons 
                             name="reply"
                             size={34}
                             onPress={this.props.closeModal}
                         />
                         
+                        {/* View which holds image */}
                         <View>
                             <Image style={styles.modalImage} source={require('../assets/Music.png')}/>
-                            <Text style={styles.modalSongName}>Song name</Text>
                         </View>
 
-                        <View>
-                            <Text>{this.props.songID}</Text>
-                        </View>
+                        {/* View which holds text with audio full name passed by props */}
+                        <View style={styles.detailsContainer}>
+					        <Text style={styles.text}>
+						        {this.props.playbackInstanceName}
+					        </Text>
+				        </View>
+				
+                        {/* View that holds audio player time info and slider */}
+                        <View
+                            style={[
+                                styles.playbackContainer,
+                                {
+                                    opacity: this.props.isLoading
+                                        ? DISABLED_OPACITY
+                                        : 1.0,
+                                },
+                            ]}
+                        >
+                            {/* Text that shows audio timestamp passed by props */}
+                            <Text style={styles.timeStampText}>
+                                {this.props.isBuffering ? (
+                                    BUFFERING_STRING
+                                    ) : (
+                                    this.props.getTimestamp
+                                )}
+                            </Text>
+
+                            {/* Slider which shows audio progress and can be used to
+                                seek forward or backward in song by using props
+                            */}
+                            <Slider
+                                style={styles.playbackSlider}
+                                value={this.props.getSeekSliderPosition}
+                                onValueChange={this.props.onSeekSliderValueChange}
+                                onSlidingComplete={this.props.onSeekSliderSlidingComplete}
+                                thumbTintColor="#000000"
+                                minimumTrackTintColor="#4CCFF9"
+                                disabled={this.props.isLoading}
+                            />
+
+                            {/* Text that shows full duration of song passed by props */}
+                            <Text style={styles.timeStampText}>
+                                {this.props.isBuffering ? (
+                                    BUFFERING_STRING
+                                ) : (
+                                    this.props.getDuration
+                                )}
+                            </Text>
+				        </View>
+
+                        {/* View that holds audio player control buttons */}
+                        <View
+                            style={[
+                                styles.buttonsContainerBase,
+                                {
+                                    opacity: this.props.isLoading
+                                        ? DISABLED_OPACITY
+                                        : 1.0,
+                                },
+                            ]}
+                        >
+                            {/* Previous song button that jumps back to previous song by using props */}
+                            <TouchableHighlight
+                                underlayColor={BACKGROUND_COLOR}
+                                style={styles.wrapper}
+                                onPress={this.props.onBackPressed}
+                                disabled={this.props.isLoading}
+                            >
+                                <View>
+                                    <MaterialIcons
+                                        name="fast-rewind"
+                                        size={40}
+                                        color="darkcyan"
+                                    />
+                                </View>
+					        </TouchableHighlight>
+                            {/* Play/Pause button that toggles between playing and pausing song using props */}
+                            <TouchableHighlight
+                                underlayColor={BACKGROUND_COLOR}
+                                style={styles.wrapper}
+                                onPress={this.props.onPlayPausePressed}
+                                disabled={this.props.isLoading}
+                            >
+                                <View>
+                                    {this.props.isPlaying ? (
+                                        <MaterialIcons
+                                            name="pause"
+                                            size={40}
+                                            color="darkcyan"
+                                        />
+                                    ) : (
+                                        <MaterialIcons
+                                            name="play-arrow"
+                                            size={40}
+                                            color="darkcyan"
+                                        />
+                                    )}
+                                </View>
+                            </TouchableHighlight>
+                            {/* Next button used to go to next song by using props */}
+                            <TouchableHighlight
+                                underlayColor={BACKGROUND_COLOR}
+                                style={styles.wrapper}
+                                onPress={this.props.onForwardPressed}
+                                disabled={this.props.isLoading}
+                            >
+                                <View>
+                                    <MaterialIcons
+                                        name="fast-forward"
+                                        size={40}
+                                        color="darkcyan"
+                                    />
+                                </View>
+                            </TouchableHighlight>
+				        </View>
+
                     </View>
                 </Modal>
-
             </View>
         )
     }
@@ -46,19 +164,51 @@ export default class Tracks extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#ccfffd',
+        flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		alignSelf: 'stretch',
+        backgroundColor: BACKGROUND_COLOR,
+        height: '100%'
     },
     modalImage: {
-        paddingTop: 30,
+        marginTop: 30,
         alignSelf: 'center',
-        height: '70%',
-        width: '70%',
+        height: DEVICE_HEIGHT / 1.8,
+        width: DEVICE_WIDTH / 1.2,
     },
-    modalSongName: {
-        paddingTop: 20,
-        alignSelf: 'center',
-        fontSize: 26,
-        height: 60
-    }, 
+    detailsContainer: {
+		height: 40,
+		marginTop: 40,
+		alignItems: 'center',
+	},
+	playbackContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+		alignItems: 'center',
+        alignSelf: 'stretch',
+        height: 50,
+	},
+	playbackSlider: {
+		alignSelf: 'stretch',
+		margin: 10,
+        width: DEVICE_WIDTH / 1.5,
+        height: 30,
+    },
+    timeStampText: {
+        margin: 10,
+    },
+	text: {
+		fontSize: FONT_SIZE,
+        minHeight: FONT_SIZE,
+        fontWeight: 'bold',
+	},
+	buttonsContainerBase: {
+		flexDirection: 'row',
+		alignItems: 'center',
+        justifyContent: 'space-between',
+        minWidth: DEVICE_WIDTH / 2.0,
+		maxWidth: DEVICE_WIDTH / 2.0,
+        marginBottom: 30
+	},
   });
--- screens/tracks.js
+++ screens/tracks.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
 import * as Permissions from 'expo-permissions';
 import { Audio } from 'expo-av';
 
 import SongModal from '../components/songModal';
 
+//Variable used in many places
+const LOADING_STRING = 'Loading...';
+
 export default class Tracks extends Component {
     constructor() {
         super();
 
+        this.index = 0;
+		this.isSeeking = false;
+		this.shouldPlayAtEndOfSeek = false;
+		this.playbackInstance = null;
         this.state = {
             songs: [],
             modalVisible: false,
             songID: "",
-            // songName: "",
-            // songDuration: null,
-            // songPath: "",
+            playbackInstanceName: LOADING_STRING,
+			playbackInstancePosition: null,
+			playbackInstanceDuration: null,
+			shouldPlay: false,
+			isPlaying: false,
+			isBuffering: false,
+			isLoading: true,
+			volume: 1.0,
+			rate: 1.0,
+			portrait: null,
         }
 
         this.closeModal = this.closeModal.bind(this);
@@ -33,22 +47,226 @@ export default class Tracks extends Component {
         })
         //Sets found audio files in state in array
         await this.setState({songs: media.assets});
+
+        //Starting audio mode using Expo.Audio
+        Audio.setAudioModeAsync({
+			allowsRecordingIOS: false,
+			interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+			playsInSilentModeIOS: true,
+			shouldDuckAndroid: true,
+			interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+        });
+    }
+
+    //Method used to load an audio file
+    async _loadNewPlaybackInstance(playing) {
+		if (this.playbackInstance != null) {
+			await this.playbackInstance.unloadAsync();
+			this.playbackInstance.setOnPlaybackStatusUpdate(null);
+			this.playbackInstance = null;
+		}
+
+        //Finding the URI from playlist array using index
+        const source = { uri: this.state.songs[this.index].uri };
+		const initialStatus = {
+			shouldPlay: playing,
+			volume: this.state.volume,
+		};
+
+		const { sound, status } = await Audio.Sound.createAsync(
+			source,
+			initialStatus,
+			this._onPlaybackStatusUpdate
+		);
+		this.playbackInstance = sound;
+
+		this._updateScreenForLoading(false);
+    }
+    
+    //Method used when making screen ready to play sound
+    _updateScreenForLoading(isLoading) {
+		if (isLoading) {
+			this.setState({
+				isPlaying: false,
+				playbackInstanceName: LOADING_STRING,
+				playbackInstanceDuration: null,
+				playbackInstancePosition: null,
+				isLoading: true,
+			});
+		} else {
+			this.setState({
+				playbackInstanceName: this.state.songs[this.index].filename,
+				isLoading: false,
+			});
+		}
+	}
+
+    //Method used to manipulate audio when it is playing
+    _onPlaybackStatusUpdate = status => {
+		if (status.isLoaded) {
+			this.setState({
+				playbackInstancePosition: status.positionMillis,
+				playbackInstanceDuration: status.durationMillis,
+				shouldPlay: status.shouldPlay,
+				isPlaying: status.isPlaying,
+				isBuffering: status.isBuffering,
+				volume: status.volume,
+            });
+            //When song finishes it jumps to next song in playlist array
+			if (status.didJustFinish) {
+				this._advanceIndex(true);
+				this._updatePlaybackInstanceForIndex(true);
+			}
+		} else {
+			if (status.error) {
+				console.log(`FATAL PLAYER ERROR: ${status.error}`);
+			}
+		}
+    };
+    
+    //Method to jump o next song using indexes in array
+    _advanceIndex(forward) {
+		this.index =
+			(this.index + (forward ? 1 : this.state.songs.length - 1)) %
+			this.state.songs.length;
+	}
+
+    //Calling methods or updating screen data and loading new song in PlaybackInstance
+    async _updatePlaybackInstanceForIndex(playing) {
+		this._updateScreenForLoading(true);
+
+		this._loadNewPlaybackInstance(playing);
+    }
+    
+    //Method used to toggle between playing song or pausing
+    _onPlayPausePressed = () => {
+		if (this.playbackInstance != null) {
+			if (this.state.isPlaying) {
+				this.playbackInstance.pauseAsync();
+			} else {
+				this.playbackInstance.playAsync();
+			}
+		}
+    };
+
+    //Method used to go to next song in array
+	_onForwardPressed = () => {
+		if (this.playbackInstance != null) {
+			this._advanceIndex(true);
+			this._updatePlaybackInstanceForIndex(this.state.shouldPlay);
+		}
+	};
+
+    //Method used to go back to previous song in array
+	_onBackPressed = () => {
+		if (this.playbackInstance != null) {
+			this._advanceIndex(false);
+			this._updatePlaybackInstanceForIndex(this.state.shouldPlay);
+		}
+	};
+
+    //Method used to pause sound and find value on slider when using slider
+	_onSeekSliderValueChange = async value => {
+		if (this.playbackInstance != null && !this.isSeeking) {
+			this.isSeeking = true;
+			this.shouldPlayAtEndOfSeek = this.state.shouldPlay;
+			this.playbackInstance.pauseAsync();
+		}
+	};
+
+    //Method used to start song from position(value) from slider
+	_onSeekSliderSlidingComplete = async value => {
+		if (this.playbackInstance != null) {
+			this.isSeeking = false;
+			const seekPosition = value * this.state.playbackInstanceDuration;
+			if (this.shouldPlayAtEndOfSeek) {
+				this.playbackInstance.playFromPositionAsync(seekPosition);
+			} else {
+				this.playbackInstance.setPositionAsync(seekPosition);
+			}
+		}
+	};
+
+    //Method used to find adn set slider positino
+	_getSeekSliderPosition() {
+		if (
+			this.playbackInstance != null &&
+			this.state.playbackInstancePosition != null &&
+			this.state.playbackInstanceDuration != null
+		) {
+			return (
+				this.state.playbackInstancePosition /
+				this.state.playbackInstanceDuration
+			);
+		}
+		return 0;
+    }
+    
+    //Method used to get milliseconds of audio file and show then as minutes:seconds
+    _getMMSSFromMillis(millis) {
+		const totalSeconds = millis / 1000;
+		const seconds = Math.floor(totalSeconds % 60);
+		const minutes = Math.floor(totalSeconds / 60);
+
+		const padWithZero = number => {
+			const string = number.toString();
+			if (number < 10) {
+				return '0' + string;
+			}
+			return string;
+		};
+		return padWithZero(minutes) + ':' + padWithZero(seconds);
+	}
+
+    //Method used to show sound current play time
+	_getTimestamp() {
+		if (
+			this.playbackInstance != null &&
+			this.state.playbackInstancePosition != null &&
+			this.state.playbackInstanceDuration != null
+		) {
+			return `${this._getMMSSFromMillis(
+				this.state.playbackInstancePosition
+			)}`;
+		}
+		return '';
+    }
+  
+    //Method used to get audio full duration 
+    _getDuration() {
+    if (
+			this.playbackInstance != null &&
+			this.state.playbackInstancePosition != null &&
+			this.state.playbackInstanceDuration != null
+		) {
+			return `${this._getMMSSFromMillis(
+				this.state.playbackInstanceDuration
+			)}`;
+		}
+		return '';
     }
 
     //Method used by child component to toggle state and close modal
     closeModal() {
         this.setState(state => ({
-            modalVisible: !state.modalVisible
+            modalVisible: !state.modalVisible,
         }));
+        this.index = 0;
     }
 
     //Metho used to open modal, update state with song info by the ID which song was clikked
     openModal = (songID) => {
+
+        this._loadNewPlaybackInstance(false)
+        
         this.setState({modalVisible: true})
 
         for (const item of this.state.songs) {
             if (item.id === songID) {
-                this.setState({songName: item.filename, songDuration: item.duration, songPath: item.uri, songID: item.id})
+
+                this.index = this.state.songs.findIndex(songs => songs.id === songID);
+
+                this.setState({songID: item.id})
             }
         }
     }
@@ -63,9 +281,18 @@ export default class Tracks extends Component {
                     IsVisible={this.state.modalVisible} 
                     songs={this.state.songs}
                     songID={this.state.songID}
-                    // songName={this.state.songName}
-                    // songDuration={this.state.songDuration}
-                    // songPath={this.state.songPath}
+                    playbackInstanceName={this.state.playbackInstanceName.split('.')[0]}
+                    isLoading={this.state.isLoading}
+                    isBuffering={this.state.isBuffering}
+                    isPlaying={this.state.isPlaying}
+                    getTimestamp={this._getTimestamp()}
+                    getSeekSliderPosition={this._getSeekSliderPosition()}
+                    onSeekSliderValueChange={this._onSeekSliderValueChange}
+                    onSeekSliderSlidingComplete={this._onSeekSliderSlidingComplete}
+                    getDuration={this._getDuration()}
+                    onBackPressed={this._onBackPressed}
+                    onPlayPausePressed={this._onPlayPausePressed}
+                    onForwardPressed={this._onForwardPressed}
                 />
 
                 {/* List with scrollbar that shows all audio files */}
@@ -74,6 +301,8 @@ export default class Tracks extends Component {
                     renderItem={({item}) => 
                         // Touchable item which toggles state and opens up modal
                         <TouchableOpacity key={item.id} style={styles.songView} onPress={() => {this.openModal(item.id)}}>
+                            <Image style={styles.tracksImages} source={require('../assets/audio_icon.png')} />
+
                             <View style={styles.songText}>
                                 <Text style={{fontWeight: 'bold'}}>{item.filename.split(".")[0].split(" - ")[1]}</Text>
                                 <Text>{item.filename.split(".")[0].split(" - ")[0]}</Text>
@@ -112,5 +341,9 @@ const styles = StyleSheet.create({
         paddingVertical: 15,
         paddingLeft: 10,
     },
-    
+    tracksImages: {
+        height: 50,
+        width: 50,
+        margin: 5,
+    }
   });
